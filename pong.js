964ffbe575bab57a4005171c867b78490ece60fe
Maria CHEREL SOROKINA <mcherel-@paul-f3Ar3s10.clusters.42paris.fr>
2023-10-12 11:41:04 +0200

    final version

--- pong.js
+++ pong.js
@@ -1,8 +1,7 @@
 /* COLORS */
-const colorPayer1 = 'blue';
-const colorPayer2 = 'red';
+let colorPlayer1 = "blue";//changable
+let colorPlayer2 = "red";//changable
 const fontStyle = "45px Courier New";
-const backgroundColor = "green";
 const ballColor = "yellowgreen"
 const defaultColor = "white";
 /* VALUES */
@@ -22,6 +21,8 @@ const computerLevel = 0.1;
 
 const roundsMax = 2;
 
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
 
 /* SELECT CANVAS */
 const canvas = document.getElementById("pong");
@@ -39,7 +40,7 @@ const user = {
     y: canvas.height/2 - paddleHeight/2,
     width: paddleWidth,
     height: paddleHeight,
-    color: colorPayer1,
+    color: colorPlayer1,
     score: 0
 }
 
@@ -50,7 +51,7 @@ const com = {
     y: canvas.height/2 - paddleHeight/2,
     width: paddleWidth,
     height: paddleHeight,
-    color: colorPayer2,
+    color: colorPlayer2,
     score: 0
 }
 
@@ -58,7 +59,7 @@ const com = {
 
 const ball = {
     x: canvas.width/2,
-    y: canvas.height/2,
+    y: canvas.height - ballRadius ,
     radius: ballRadius,
     speed: ballSpeed,
     color: ballColor,
@@ -107,8 +108,6 @@ function drawCircle(x, y, r, color){
     context.fill();
 }
 
-//drawCircle(100, 100, 50, defaultColor);
-
 /* DRAW TEXT */
 
 function drawText(text, x, y, color){
@@ -116,21 +115,21 @@ function drawText(text, x, y, color){
     context.font = fontStyle;
     context.fillText(text, x, y);
 }
-//drawText("something", 300, 200, defaultColor);
+
 
 
 /* RENDER THE GAME */
 function render(){
     //clear canvas
-    drawRect(0, 0, canvas.width/2, canvas.height, colorPayer2);
-    drawRect(canvas.width/2, 0, canvas.width, canvas.height, colorPayer1);
+    drawRect(0, 0, canvas.width/2, canvas.height, colorPlayer2);
+    drawRect(canvas.width/2, 0, canvas.width, canvas.height, colorPlayer1);
     //draw net
     drawNet();
     //draw obstacle
     drawRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height, obstacle.color);
     //draw score
-    drawText(user.score, canvas.width/4, canvas.height/5, colorPayer1);
-    drawText(com.score, (3 * canvas.width/4), canvas.height/5, colorPayer2);
+    drawText(user.score, canvas.width/4, canvas.height/5, colorPlayer1);
+    drawText(com.score, (3 * canvas.width/4), canvas.height/5, colorPlayer2);
     //draw paddles
     drawRect(user.x, user.y, user.width, user.height, user.color);
     drawRect(com.x, com.y, com.width, com.height, com.color);
@@ -140,12 +139,8 @@ function render(){
 }
 
 canvas.addEventListener("mousemove", movePaddle);
-//canvas.addEventListener("touchmove", movePaddle);//for the phone ...
 canvas.addEventListener("keydown", setInterval);
 
-/* function sayHello(){
-    console.log("Hello");
-} */
 function movePaddle(evt){
     let rect = canvas.getBoundingClientRect();
 
@@ -156,13 +151,44 @@ function movePaddle(evt){
 function resetBall(player){
     ball.speed = ballSpeed;
     ball.x = canvas.width/2;
-    ball.y = canvas.height/2;
+    ball.y = canvas.height - ballRadius;
 
     if (player != com)
-        ball.velocityX = -ball.velocityX; 
-    //ball.velocityY = -ball.velocityY;
+        ball.velocityX = -ball.velocityX;
     
 }
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+}
+
+function obstacleCollision(ball, obstacle, player) {
+    if (
+        ball.x + ball.radius > obstacle.x && // Ball's right edge is to the right of the obstacle's left edge
+        ball.x - ball.radius < obstacle.x + obstacle.width && // Ball's left edge is to the left of the obstacle's right edge
+        ball.y + ball.radius > obstacle.y && // Ball's bottom edge is below the obstacle's top edge
+        ball.y - ball.radius < obstacle.y + obstacle.height // Ball's top edge is above the obstacle's bottom edge
+    ) {
+        // CHanging the drection
+
+        ball.velocityX = -ball.velocityX;
+        ball.velocityY = -ball.velocityY;
+        
+
+        //changing the opposite side color
+        let hexColor = '#';
+        for (let i = 0; i < 6; i++){
+            hexColor += hex[getRandomNumber()];
+        }
+        if (player === user)
+            com.color = colorPlayer2 = hexColor;
+        if (player === com)
+            user.color = colorPlayer1 = hexColor;
+        console.log(hexColor);
+    }
+}
+
+
 
 function collision(ball, paddle){
     ball.top = ball.y - ball.radius;
@@ -192,6 +218,8 @@ function gameUpdate(){
 
     
     let player = (ball.x + ball.radius < canvas.width/2) ? user : com;
+
+    obstacleCollision(ball, obstacle, player);
     
     if (collision( ball, player)){
         //where the ball hit the player
@@ -226,8 +254,8 @@ function gameUpdate(){
         resetBall(user);}
     if (com.score === roundsMax || user.score === roundsMax){
         game.running = false;
-        user.color = colorPayer2;
-        com.color = colorPayer1;
+        user.color = colorPlayer2;
+        com.color = colorPlayer1;
 
     }
 
@@ -264,7 +292,7 @@ function start (){
     else
         message = startMessage;
  
-    // Draw the 'press any key to begin' text
+    // Draw the "press any key to begin" text
     context.textAlign = "center";
     context.fillText(message,
     canvas.width/2,
